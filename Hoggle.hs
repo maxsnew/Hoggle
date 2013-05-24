4f51035f26fc206a97753bf2bb7c175d1e67a112
Max New <maxsnew@gmail.com>
2013-05-24 12:38:59 -0500

    Expanded CLI, added ListTrie dependency.

--- Hoggle.hs
+++ Hoggle.hs
@@ -4,7 +4,29 @@ module Main
 import Game.Hoggle
 
 import Control.Monad.State
+import Data.List as List
+import System.Environment
 import System.Random
 
 main :: IO ()
-main = getStdGen >>= print . evalState randBoard 
+main = do
+  args <- getArgs
+  case args of
+    []         -> badCmd
+    sub : flags -> dispatch sub flags
+
+dispatch :: String -> [String] -> IO ()
+dispatch cmd args = sub
+  where sub = maybe badCmd ($ args) . List.lookup cmd $ subcommands
+
+subcommands :: [(String, [String] -> IO ())]
+subcommands = [("play", play), ("load", load)]  
+
+play :: [String] -> IO ()
+play _ = getStdGen >>= putStr . show . evalState randBoard
+
+load :: [String] -> IO ()
+load _ = putStrLn "Unimplemented: load"
+
+badCmd :: IO ()
+badCmd = putStrLn "Expected subcommand: [play, load]"
---
+++ src/Game/Hoggle/Trie.hs
@@ -0,0 +1,8 @@
+module Game.Hoggle.Trie
+       where
+
+import Data.Map as Map
+import Data.ListTrie.Set as Set
+
+type Dict = TrieSet Map Char
+
