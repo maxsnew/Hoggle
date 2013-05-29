c515f2c42181762521b568d222e3eb950a8b0874
Max New <maxsnew@gmail.com>
2013-05-29 11:47:03 -0500

    Fixed warnings and changed base dependency.

--- src/Game/Hoggle/Words.hs
+++ src/Game/Hoggle/Words.hs
@@ -2,7 +2,7 @@ module Game.Hoggle.Words
        where
 
 isValid :: Int -> Int -> String -> Bool
-isValid min max s = (length s >= min) && (length s < max) && notProper
+isValid mn mx s = (length s >= mn) && (length s < mx) && notProper
   where notProper = case s of
           "" -> True
           _  -> head s `elem` ['a'..'z']
